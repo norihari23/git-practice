fabd7bc865929891d6ca5c1d3678a20c189a8812
srihari <norihari23@gmail.com>
2024-08-26 17:43:39 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,15 +4,20 @@
  #echo "User ID is: $USERID"
 
  if[ $USERID -ne 0 ]
+ 
  then
+
     echo "Please run this script with root privileges"
+
    exit 1
 fi
 
  dnf list installed git 
 
  if[ $? -ne 0 ]
+ 
  then
+
       echo "Git is not installed, going to install it.."
       dnf install git -y
       if [ $? -ne 0 ]
@@ -30,16 +35,23 @@ fi
 dnf list installed mysql
 
 if [ $? -ne 0 ]
+
 then  echo "MySQL is not installed..going to install"
+
 dnf install mysql -y
+
 if [ $? -ne 0 ] 
+
 then
+
    echo "MYSQL installation is failure..please check"
+
    exit 1
+   
    else
      echo "MYSQL installation is success"
      fi
      else
      echo "MYSQL is already installed..nothing to do"
      fi
-     
+
